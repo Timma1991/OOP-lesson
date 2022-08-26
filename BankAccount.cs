9a70c378a0840c5a51ec84f6398b842f937d01d1
Lebedev Timofey <lebedev.timofey007@gmail.com>
2022-08-26 19:51:29 +0300

    done working on lesson

---
+++ BankAccount.cs
@@ -0,0 +1,56 @@
+﻿namespace BankCell
+{
+    internal class BankAccount
+    {
+        private int balance = 0;
+        private BalanceType accountType;
+        private long accountNumber = 0;
+        public static long AccountNumberGlobal = 207025200;
+
+        public BankAccount()
+        {
+            this.accountNumber = GenerateAccountNumber();
+        }
+
+        public BankAccount(BalanceType accountType)
+        {
+            this.accountType = accountType;
+            this.accountNumber = GenerateAccountNumber();
+        }
+
+        public BankAccount(int balance, BalanceType typescoretype)
+        {
+            this.balance = balance;
+            this.accountType = typescoretype;
+            this.accountNumber = GenerateAccountNumber();
+        }
+
+        public long GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+        public BalanceType GetAccountType()
+        {
+            return accountType;
+        }
+
+        public override string? ToString()
+        {
+            return 
+                $"Balance: {GetBalance()}\n" +
+                $"AccountType: {GetAccountType()}\n" +
+                $"AccountNumber: {GetAccountNumber()}";
+        }
+
+        private long GenerateAccountNumber()
+        {
+            return AccountNumberGlobal++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankCell
+{
+    internal class Programm
+    {
+        
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(1,BalanceType.Credit);
+            
+            Console.WriteLine(account);
+
+            BankAccount.AccountNumberGlobal = 5;
+
+
+        }
+    }
+}
