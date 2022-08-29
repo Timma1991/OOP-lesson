c4664b364e638b8ca3fc3a2095eddf1ec7113853
Lebedev Timofey <lebedev.timofey007@gmail.com>
2022-08-29 15:38:50 +0300

    Испрвил все недочеты к дз

--- BankAccount.cs
+++ BankAccount.cs
@@ -2,52 +2,47 @@
 {
     internal class BankAccount
     {
-        private int balance = 0;
-        private BalanceType accountType;
-        private long accountNumber = 0;
+        public long Balance { get; set; }
+        public BalanceType AccountType { get; set; }
+        public long AccountNumber { get; set; }
         public static long AccountNumberGlobal = 207025200;
 
         public BankAccount()
         {
-            this.accountNumber = GenerateAccountNumber();
+            this.AccountNumber = GenerateAccountNumber();
         }
-
         public BankAccount(BalanceType accountType)
         {
-            this.accountType = accountType;
-            this.accountNumber = GenerateAccountNumber();
+            this.AccountType = accountType;
+            this.AccountNumber = GenerateAccountNumber();
         }
-
         public BankAccount(int balance, BalanceType typescoretype)
         {
-            this.balance = balance;
-            this.accountType = typescoretype;
-            this.accountNumber = GenerateAccountNumber();
-        }
-
-        public long GetAccountNumber()
+            this.Balance = balance;
+            this.AccountType = typescoretype;
+            this.AccountNumber = GenerateAccountNumber();
+        }           
+        public bool Credit(long amount)
         {
-            return accountNumber;
-        }
-
-        public int GetBalance()
+            if(Balance>=amount)
+            {
+                Balance -= amount;
+                return true;
+            }   
+            return false;
+        }        
+        public void Debit(long amount)
         {
-            return balance;
-        }
-
-        public BalanceType GetAccountType()
-        {
-            return accountType;
+            Balance+=amount;
         }
 
         public override string? ToString()
         {
             return 
-                $"Balance: {GetBalance()}\n" +
-                $"AccountType: {GetAccountType()}\n" +
-                $"AccountNumber: {GetAccountNumber()}";
+                $"Balance: {Balance}\n" +
+                $"AccountType: {AccountType}\n" +
+                $"AccountNumber: {AccountNumber}";
         }
-
         private long GenerateAccountNumber()
         {
             return AccountNumberGlobal++;
